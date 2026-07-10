b4e2e99697ed35665918c6c82c94fc2310b0f3f7
Alex Gatea <alexgatea@ibm.com>
2026-07-10 15:24:10 -0400

    Update LinalgLowering to use a type-switch
    Signed-off-by: Alex Gatea <alexgatea@ibm.com>

--- lib/Conversion/backend/ScheduleIRToDFIR/KTDFLowToDFIR/LinalgLowering.cpp
+++ lib/Conversion/backend/ScheduleIRToDFIR/KTDFLowToDFIR/LinalgLowering.cpp
@@ -26,6 +26,7 @@
 #include "dataflow-scheduler/Conversion/backend/ScheduleIRToDFIR/KTDFLowToDFIR/Utils.h"
 #include "dataflow-scheduler/Dialect/VectorChain/VectorChain.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/TypeSwitch.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/IR/AffineMap.h"
@@ -77,56 +78,29 @@ struct LowerLinalgGenericPattern
       ops_to_lower.push_back(&op);
     }
 
-    // Process and lower compute operations
+    // Process and lower compute operations via visitors
     rewriter.setInsertionPoint(generic_op);
     for (mlir::Operation* op : ops_to_lower) {
-      // arith.mulf %lhs, %rhs -> vectorchain.binary {binary_op = mul}
-      if (auto mulf_op = mlir::dyn_cast<mlir::arith::MulFOp>(op)) {
-        auto vector_type =
-            getFlattenedVectorType(mulf_op.getLhs().getType(), resource_kinds_);
-        if (!vector_type) return mlir::failure();
+      mlir::LogicalResult result =
+          llvm::TypeSwitch<mlir::Operation*, mlir::LogicalResult>(op)
+              // arith.mulf %lhs, %rhs -> vectorchain.binary {binary_op = mul}
+              .Case<mlir::arith::MulFOp>([&](mlir::arith::MulFOp mulf_op) {
+                return lowerMulFOp(mulf_op, rewriter, identity_map);
+              })
+              // arith.addf %lhs, %rhs -> vectorchain.binary {binary_op = add}
+              .Case<mlir::arith::AddFOp>([&](mlir::arith::AddFOp addf_op) {
+                return lowerAddFOp(addf_op, rewriter, identity_map);
+              })
+              // arith.subf %lhs, %rhs -> vectorchain.binary {binary_op = sub}
+              .Case<mlir::arith::SubFOp>([&](mlir::arith::SubFOp subf_op) {
+                return lowerSubFOp(subf_op, rewriter, identity_map);
+              })
+              .Default([](mlir::Operation* unknown_op) {
+                return unknown_op->emitError(
+                    "unsupported operation type in linalg.generic body");
+              });
 
-        auto binary_op = mlir::vectorchain::BinaryOp::create(
-            rewriter, mulf_op.getLoc(), vector_type, mulf_op.getLhs(),
-            mulf_op.getRhs(),
-            /*mask=*/nullptr, /*dbgName=*/nullptr,
-            mlir::vectorchain::VectorChainBinaryOperator::mul, identity_map);
-
-        rewriter.replaceOp(mulf_op, binary_op.getData());
-        continue;
-      }
-
-      // arith.addf %lhs, %rhs -> vectorchain.binary {binary_op = add}
-      if (auto addf_op = mlir::dyn_cast<mlir::arith::AddFOp>(op)) {
-        auto vector_type =
-            getFlattenedVectorType(addf_op.getLhs().getType(), resource_kinds_);
-        if (!vector_type) return mlir::failure();
-
-        auto binary_op = mlir::vectorchain::BinaryOp::create(
-            rewriter, addf_op.getLoc(), vector_type, addf_op.getLhs(),
-            addf_op.getRhs(),
-            /*mask=*/nullptr, /*dbgName=*/nullptr,
-            mlir::vectorchain::VectorChainBinaryOperator::add, identity_map);
-
-        rewriter.replaceOp(addf_op, binary_op.getData());
-        continue;
-      }
-
-      // arith.subf %lhs, %rhs -> vectorchain.binary {binary_op = sub}
-      if (auto subf_op = mlir::dyn_cast<mlir::arith::SubFOp>(op)) {
-        auto vector_type =
-            getFlattenedVectorType(subf_op.getLhs().getType(), resource_kinds_);
-        if (!vector_type) return mlir::failure();
-
-        auto binary_op = mlir::vectorchain::BinaryOp::create(
-            rewriter, subf_op.getLoc(), vector_type, subf_op.getLhs(),
-            subf_op.getRhs(),
-            /*mask=*/nullptr, /*dbgName=*/nullptr,
-            mlir::vectorchain::VectorChainBinaryOperator::sub, identity_map);
-
-        rewriter.replaceOp(subf_op, binary_op.getData());
-        continue;
-      }
+      if (mlir::failed(result)) return mlir::failure();
     }
 
     // Replace the generic op with the yield operand
@@ -136,6 +110,60 @@ struct LowerLinalgGenericPattern
 
  private:
   arch_view::ResourceKinds& resource_kinds_;
+
+  // arith.mulf visitor: lowers to vectorchain.binary {binary_op = mul}
+  mlir::LogicalResult lowerMulFOp(mlir::arith::MulFOp mulf_op,
+                                  mlir::PatternRewriter& rewriter,
+                                  mlir::AffineMap identity_map) const {
+    auto vector_type =
+        getFlattenedVectorType(mulf_op.getLhs().getType(), resource_kinds_);
+    if (!vector_type) return mlir::failure();
+
+    auto binary_op = mlir::vectorchain::BinaryOp::create(
+        rewriter, mulf_op.getLoc(), vector_type, mulf_op.getLhs(),
+        mulf_op.getRhs(),
+        /*mask=*/nullptr, /*dbgName=*/nullptr,
+        mlir::vectorchain::VectorChainBinaryOperator::mul, identity_map);
+
+    rewriter.replaceOp(mulf_op, binary_op.getData());
+    return mlir::success();
+  }
+
+  // arith.addf visitor: lowers to vectorchain.binary {binary_op = add}
+  mlir::LogicalResult lowerAddFOp(mlir::arith::AddFOp addf_op,
+                                  mlir::PatternRewriter& rewriter,
+                                  mlir::AffineMap identity_map) const {
+    auto vector_type =
+        getFlattenedVectorType(addf_op.getLhs().getType(), resource_kinds_);
+    if (!vector_type) return mlir::failure();
+
+    auto binary_op = mlir::vectorchain::BinaryOp::create(
+        rewriter, addf_op.getLoc(), vector_type, addf_op.getLhs(),
+        addf_op.getRhs(),
+        /*mask=*/nullptr, /*dbgName=*/nullptr,
+        mlir::vectorchain::VectorChainBinaryOperator::add, identity_map);
+
+    rewriter.replaceOp(addf_op, binary_op.getData());
+    return mlir::success();
+  }
+
+  // arith.subf visitor: lowers to vectorchain.binary {binary_op = sub}
+  mlir::LogicalResult lowerSubFOp(mlir::arith::SubFOp subf_op,
+                                  mlir::PatternRewriter& rewriter,
+                                  mlir::AffineMap identity_map) const {
+    auto vector_type =
+        getFlattenedVectorType(subf_op.getLhs().getType(), resource_kinds_);
+    if (!vector_type) return mlir::failure();
+
+    auto binary_op = mlir::vectorchain::BinaryOp::create(
+        rewriter, subf_op.getLoc(), vector_type, subf_op.getLhs(),
+        subf_op.getRhs(),
+        /*mask=*/nullptr, /*dbgName=*/nullptr,
+        mlir::vectorchain::VectorChainBinaryOperator::sub, identity_map);
+
+    rewriter.replaceOp(subf_op, binary_op.getData());
+    return mlir::success();
+  }
 };
 
 }  // namespace
