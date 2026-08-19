c21e9f85a5e15d04dfef030f5f67c3c206193234
Karl F. A. Friebel <karl.friebel@ibm.com>
2026-08-19 10:20:32 +0200

    Added import tests for all dialect packages.
    Signed-off-by: Karl F. A. Friebel <karl.friebel@ibm.com>

--- test/python/import.py
+++ test/python/import.py
@@ -7,7 +7,22 @@ import mlir_scheduler
 import mlir_scheduler.ir
 
 import mlir_scheduler.dialects.arith
+import mlir_scheduler.dialects.builtin
+import mlir_scheduler.dialects.complex
+import mlir_scheduler.dialects.func
+import mlir_scheduler.dialects.index
+import mlir_scheduler.dialects.linalg
+import mlir_scheduler.dialects.math
+import mlir_scheduler.dialects.memref
+import mlir_scheduler.dialects.scf
+import mlir_scheduler.dialects.tensor
 
 import mlir_scheduler.dialects.ktdp
 
+import mlir_scheduler.dialects.agen
+import mlir_scheduler.dialects.dataflow
 import mlir_scheduler.dialects.ktdf
+import mlir_scheduler.dialects.ktdf_arch
+import mlir_scheduler.dialects.ktdf_lowering
+import mlir_scheduler.dialects.uniform
+import mlir_scheduler.dialects.vectorchain
